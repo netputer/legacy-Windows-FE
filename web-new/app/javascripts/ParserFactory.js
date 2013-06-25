0d20423a65494c13c3d0f4080c800abfd1ae9a98
Wangye Zhao <ivanzhaowy@gmail.com>
2013-06-26 07:08:18 +0800

    [Feature] Add BindView

--- web-new/app/javascripts/ParserFactory.js
+++ web-new/app/javascripts/ParserFactory.js
@@ -1,10 +1,10 @@
 /*global define*/
 (function (window, undefined) {
-    define(['underscore', 'WorkerPoll'], function (_, WorkerPoll) {
+    define(['underscore', 'WorkerPoll', 'Configuration'], function (_, WorkerPoll, CONFIG) {
         console.log('ParserFactory - File loaded.');
 
         var POOL_SIZE = 10;
-        var WORKER_PATH = require.s.contexts._.config.baseUrl + '/workers/jsonparser.js';
+        var WORKER_PATH = CONFIG.BASE_PATH + 'workers/jsonparser.js';
 
         // var ParserFactory = new WorkerPoll(WORKER_PATH, POOL_SIZE);
 
