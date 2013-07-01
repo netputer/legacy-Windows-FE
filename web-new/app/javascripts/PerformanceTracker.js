fa058fcbb35c6cb10e3557be04dbaa671dfbcbf6
Wangye Zhao <ivanzhaowy@gmail.com>
2013-07-01 21:41:44 +0800

    [Bugfix] Launch peformance tracker after DOM ready.

--- web-new/app/javascripts/PerformanceTracker.js
+++ web-new/app/javascripts/PerformanceTracker.js
@@ -2,39 +2,43 @@
 (function (window) {
     define([
         'Distributor',
-        'Log'
+        'Log',
+        'jquery'
     ], function (
         Distributor,
-        log
+        log,
+        $
     ) {
         var PerformanceTracker =  {};
 
         PerformanceTracker.launch = function () {
             if (Distributor.PERFORMANCE_TRACK) {
-                var msg = {'event' : 'debug.fe.timing'};
-                var k;
-                for (k in window.performance.timing) {
-                    if (window.performance.timing.hasOwnProperty(k)) {
-                        msg[k] = window.performance.timing[k];
+                $(function (argument) {
+                    var msg = {'event' : 'debug.fe.timing'};
+                    var k;
+                    for (k in window.performance.timing) {
+                        if (window.performance.timing.hasOwnProperty(k)) {
+                            msg[k] = window.performance.timing[k];
+                        }
                     }
-                }
-                log(msg);
+                    log(msg);
+                });
+
+                var logMemory = function () {
+                    var msg = {'event' : 'debug.fe.memory'};
+                    var k;
+                    for (k in window.performance.memory) {
+                        if (window.performance.memory.hasOwnProperty(k)) {
+                            msg[k] = window.performance.memory[k];
+                        }
+                    }
+                    log(msg);
+                };
+
+                logMemory.call(this);
+
+                setInterval(logMemory, 1000 * 60 * 2);
             }
-
-            var logMemory = function () {
-                var msg = {'event' : 'debug.fe.memory'};
-                var k;
-                for (k in window.performance.memory) {
-                    if (window.performance.memory.hasOwnProperty(k)) {
-                        msg[k] = window.performance.memory[k];
-                    }
-                }
-                log(msg);
-            };
-
-            logMemory.call(this);
-
-            setInterval(logMemory, 1000 * 60 * 2);
         };
 
         return PerformanceTracker;
