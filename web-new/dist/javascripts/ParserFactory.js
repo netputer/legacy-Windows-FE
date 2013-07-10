cce6416a3493fb812c6b31ffdc45c0042d8d75e3
Wangye Zhao <ivanzhaowy@gmail.com>
2013-07-10 12:41:39 +0800

    Build Project

--- web-new/dist/javascripts/ParserFactory.js
+++
@@ -1,1 +0,0 @@
-(function(e,t){define(["underscore","WorkerPoll","Configuration"],function(e,t,n){console.log("ParserFactory - File loaded.");var r=10,i=n.BASE_PATH+"workers/jsonparser.js"})})(this);
