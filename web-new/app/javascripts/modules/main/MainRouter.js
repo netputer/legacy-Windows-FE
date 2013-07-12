da47102113c326583c50963bab251a8581f63b8c
Wangye Zhao <ivanzhaowy@gmail.com>
2013-07-12 16:05:09 +0800

    Revert "[Feature] Add universal historr manage function for PIM modules."
    This reverts commit 132517f36e41d876336a548317e2f6f4e6ed100e.
    
    Conflicts:
    	web-new/app/javascripts/modules/main/MainRouter.js

--- web-new/app/javascripts/modules/main/MainRouter.js
+++ web-new/app/javascripts/modules/main/MainRouter.js
@@ -1,25 +1,11 @@
 /*global define*/
 (function (window) {
     define([
-        'backbone',
-        'IOBackendDevice',
-        'Configuration',
-        'jquery',
-        'main/collections/PIMCollection'
+        'backbone'
     ], function (
-        Backbone,
-        IO,
-        CONFIG,
-        $,
-        PIMCollection
+        Backbone
     ) {
         console.log('MainRouter - File loaded. ');
-
-        var history = window.history;
-
-        var pimCollection = PIMCollection.getInstance();
-
-        var historyStack = [];
 
         var MainRouter = Backbone.Router.extend({
             routes : {
@@ -32,85 +18,16 @@
                     module : module,
                     tab : tab || ''
                 });
-
-                var target = pimCollection.find(function (model) {
-                    return (model.get('module') === module) &&
-                            (model.get('tab') === tab);
-                });
-
-                if (target === undefined) {
-                    target = pimCollection.find(function (model) {
-                        return (model.get('module') === module) && model.get('root');
-                    });
-                }
-
-                target.set('selected', true);
-                var fragment = 'main/' + module + '/' + (tab || '');
-                if (historyStack[historyStack.length - 1] !== fragment) {
-                    historyStack.push(fragment);
-                }
             },
             switchExtension : function (id, url) {
                 Backbone.trigger('switchModule:browser', {
                     id : id,
                     url : decodeURIComponent(url)
                 });
-
-                window.externalCall('', 'navigation', JSON.stringify({
-                    id : '',
-                    canGoBack : history.backCount(),
-                    canGoForward : history.forwardCount()
-                }));
             }
         });
 
         var mainRouter = new MainRouter();
-
-        IO.Backend.Device.onmessage({
-            'data.channel' : CONFIG.events.NAVIGATE_BACK
-        }, function (data) {
-            if (window.SnapPea.CurrentModule === 'browser') {
-                var $iframe = $('iframe[extension="' + window.SnapPea.CurrentTab + '"]');
-                console.log();
-                if (history.backCount($iframe.attr('id'), $iframe.attr('branch')) !== 0) {
-                    history.back2($iframe.attr('id'), $iframe.attr('branch'));
-                } else {
-                    // history.back();
-                }
-            } else {
-                console.log(history.length);
-                history.back();
-            }
-        });
-
-        IO.Backend.Device.onmessage({
-            'data.channel' : CONFIG.events.NAVIGATE_FORWARD
-        }, function (data) {
-            if (window.SnapPea.CurrentModule === 'browser') {
-                var $iframe = $('iframe[extension="' + window.SnapPea.CurrentTab + '"]');
-                if (history.forwardCount($iframe.attr('id'), $iframe.attr('branch')) !== 0) {
-                    history.forward2($iframe.attr('id'), $iframe.attr('branch'));
-                } else {
-                    // history.forward();
-                }
-            } else {
-                // history.forward();
-            }
-        });
-
-        IO.Backend.Device.onmessage({
-            'data.channel' : CONFIG.events.NAVIGATE_REFRESH
-        }, function (data) {
-        });
-
-
-        // window.onhashchange = function (e) {
-        //     console.log(e);
-        // };
-
-        // window.popstate = function (e) {
-        //     console.log(e);
-        // };
 
         return mainRouter;
     });
