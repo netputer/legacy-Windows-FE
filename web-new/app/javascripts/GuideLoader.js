1eb57e5732eb53ebc476b32a71773f2bdaa9a1ab
Wangye Zhao <ivanzhaowy@gmail.com>
2013-07-23 17:00:12 +0800

    Polish codes

--- web-new/app/javascripts/GuideLoader.js
+++ web-new/app/javascripts/GuideLoader.js
@@ -1,3 +1,4 @@
+/*global require*/
 require.config({
     paths : {
         jquery : '../bower_components/jquery/jquery',
--- web-new/app/javascripts/modules/message/views/ConversationsListView.js
+++ web-new/app/javascripts/modules/message/views/ConversationsListView.js
@@ -1,5 +1,5 @@
 /*global define*/
-(function (window, document, undefined) {
+(function (window, document) {
     define([
         'backbone',
         'underscore',
@@ -140,7 +140,7 @@
                                     silent : true
                                 });
 
-                                ThreadsPanelView.getInstance().once('rendered', function (){
+                                ThreadsPanelView.getInstance().once('rendered', function () {
                                     this.update(conversation.get('id'), msg);
                                 });
                                 conversationList.addSelect(conversation.id);
@@ -172,15 +172,15 @@
                 conversationList.toggleEmptyTip(conversationList.currentModels.length === 0);
             },
             switchListDataSet : function (tab) {
-                var tab = tab || 'default';
+                tab = tab || 'default';
                 var getter;
 
                 switch (tab) {
-                    case 'default' :
-                        getter = conversationsCollection.getAll;
+                case 'default':
+                    getter = conversationsCollection.getAll;
                     break;
-                    case 'search' :
-                        getter = conversationsCollection.getByKeyword;
+                case 'search':
+                    getter = conversationsCollection.getByKeyword;
                     break;
                 }
 
@@ -274,14 +274,14 @@
                 return deferred.promise();
             },
             showByKeyword : function () {
-                conversationsCollection.searchConversationAsync().done(function (resp){
+                conversationsCollection.searchConversationAsync().done(function (resp) {
                     this.switchListDataSet('search');
                     conversationList.deselectAll({
                         silent : true
                     });
 
                     var models = conversationsCollection.getByKeyword();
-                    if (models.length > 0 ){
+                    if (models.length > 0) {
                         var conversation = models[0];
                         conversationList.addSelect(conversation.id);
                     }
