509a5da00f9d02d18a1b205636d79e742f871150
Wangye Zhao <ivanzhaowy@gmail.com>
2013-08-16 18:51:06 +0800

    WANDOUJIA-6812

--- app/javascripts/ui/MediaInfoPanelView.js
+++ app/javascripts/ui/MediaInfoPanelView.js
@@ -18,7 +18,7 @@
         console.log('MediaInfoPanelView - File loaded. ');
 
         var MediaInfoPanelView = PopupPanel.extend({
-            className : 'w-ui-popup-tip .w-layout-hide',
+            className : 'w-ui-popup-tip w-layout-hide',
             initialize : function () {
                 MediaInfoPanelView.__super__.initialize.apply(this, arguments);
                 this.$content = doT.template(TemplateFactory.get('photo', 'info'))(this.model.toJSON());
