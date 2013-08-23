57c04f6537c5e2e9b57a97b945242fdf18714cbd
NetPuter Lin <netputer@gmail.com>
2013-08-23 14:35:10 +0800

    [Polish] update view, only bold primary text

--- app/javascripts/nls/en/app.js
+++ app/javascripts/nls/en/app.js
@@ -189,8 +189,8 @@ define({
 
     UPDATE_FROM_SNAPPEA : '来自豌豆荚的升级',
     UPDATE_FROM_OTHERS : '来自其它市场的升级',
-    RECOMMENDED_UPDATE : '来自豌豆荚的升级 - 经过豌豆荚审核',
-    NOT_RECOMMENDED_UPDATE : '来自其他市场的升级 - 未经豌豆荚审核',
+    RECOMMENDED_UPDATE : '<strong>来自豌豆荚的升级</strong> - 经过豌豆荚审核',
+    NOT_RECOMMENDED_UPDATE : '<strong>来自其它市场的升级</strong> - 未经豌豆荚审核',
 
     /* Permission info */
     "android.permission-group.ACCOUNTS" : "访问您的帐户",
