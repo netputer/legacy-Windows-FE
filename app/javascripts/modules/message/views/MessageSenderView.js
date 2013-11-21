84aaccfce20c67d0ddbcd78087e5566989920698
Zeke Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2013-11-21 13:11:59 +0800

    [Bugfix] Fit backend events data struct of direct sending SMS through click search result item.

--- app/javascripts/modules/message/views/MessageSenderView.js
+++ app/javascripts/modules/message/views/MessageSenderView.js
@@ -121,7 +121,6 @@
                 if (contact instanceof Array) {
                     var fragment = document.createDocumentFragment();
                     _.each(contact, function (con) {
-
                         var model = con.id ? contactMultiNumbersCollection.get(con.id) : con;
                         if (receiverList.indexOf(model.get('phoneNumber')) < 0) {
                             receiverList.push(model.get('phoneNumber'));
@@ -576,6 +575,9 @@
                         sendHandler.call(this);
                     }
                 }
+            },
+            addContactById : function (id) {
+                senderBodyView.addContact(contactMultiNumbersCollection.get(id));
             },
             clickButtonSend : function () {
                 this.sendMessage();
