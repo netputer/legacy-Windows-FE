fcfed494851641b1bc33c662a23797f255c93790
NetPuter <NetPuter@Gmail.com>
2014-01-11 21:31:02 +0800

    [Polish] Limit nickname length

--- app/javascripts/modules/welcome/views/ClockView.js
+++ app/javascripts/modules/welcome/views/ClockView.js
@@ -64,7 +64,7 @@
                 this.$el.html(this.template({
                     isLogin : Account.get('isLogin'),
                     greeting : greeting,
-                    userName : Account.get('userName')
+                    userName : Account.get('userName').length > 8 ? Account.get('userName').substr(0, 7) + '…' : Account.get('userName')
                 })).toggleClass('logged-in', Account.get('isLogin'));
 
                 this.renderContent();
