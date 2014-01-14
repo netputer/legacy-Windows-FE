0129f8dd8b41973b9d26cd277023bd449fbfb502
lixiaopeng <lixiaopeng@wandoujia.com>
2014-01-15 01:05:27 +0800

    [Feature] add debug module for native developer

--- Gruntfile.js
+++ Gruntfile.js
@@ -146,39 +146,46 @@ module.exports = function (grunt) {
             }
         },
         requirejs : {
+            debug : {
+                options : {
+                    optimize : 'none'
+                }
+            },
             source : {
                 options : {
-                    almond : true,
-                    appDir : '<%= path.tmp %>/javascripts',
-                    dir :　'<%= path.dist %>/javascripts',
                     optimize : 'uglify',
-                    baseUrl : './',
-                    mainConfigFile : '<%= path.tmp %>/javascripts/RequireConfig.js',
                     uglify : {
                         toplevel : true,
                         ascii_only : false,
                         beautify : false
-                    },
-                    preserveLicenseComments : true,
-                    useStrict : false,
-                    wrap : true,
-                    modules : [{
-                        name : 'RequireConfig',
-                        include : ['jquery', 'underscore', 'backbone', 'doT', 'text', 'i18n']
-                    }, {
-                        name : 'SnapPea',
-                        include : ['SnapPea'],
-                        exclude : ['RequireConfig']
-                    }, {
-                        name : 'photo/PhotoModule',
-                        include : ['photo/PhotoModule'],
-                        exclude : ['RequireConfig']
-                    }, {
-                        name : 'welcome/guide/views/GuideView',
-                        include : ['welcome/guide/views/GuideView'],
-                        exclude : ['RequireConfig']
-                    }]
+                    }
                 }
+            },
+            options : {
+                almond : true,
+                appDir : '<%= path.tmp %>/javascripts',
+                dir :　'<%= path.dist %>/javascripts',
+                baseUrl : './',
+                mainConfigFile : '<%= path.tmp %>/javascripts/RequireConfig.js',
+                preserveLicenseComments : true,
+                useStrict : false,
+                wrap : true,
+                modules : [{
+                    name : 'RequireConfig',
+                    include : ['jquery', 'underscore', 'backbone', 'doT', 'text', 'i18n']
+                }, {
+                    name : 'SnapPea',
+                    include : ['SnapPea'],
+                    exclude : ['RequireConfig']
+                }, {
+                    name : 'photo/PhotoModule',
+                    include : ['photo/PhotoModule'],
+                    exclude : ['RequireConfig']
+                }, {
+                    name : 'welcome/guide/views/GuideView',
+                    include : ['welcome/guide/views/GuideView'],
+                    exclude : ['RequireConfig']
+                }]
             }
         },
         useminPrepare : {
@@ -249,10 +256,15 @@ module.exports = function (grunt) {
         grunt.task.run(taskList);
     });
 
-    grunt.registerTask('build', function (project) {
+    grunt.registerTask('build', function (project, requireTask) {
 
         if (typeof project !== 'undefined') {
             project_flag = project.toUpperCase();
+        }
+
+        var rtask = 'source';
+        if (typeof requireTask !== 'undefined') {
+            rtask = requireTask;
         }
 
         var taskList = [
@@ -261,7 +273,7 @@ module.exports = function (grunt) {
             'replace:' + project_flag,
             'createScssConfig',
             'compass:dist',
-            'requirejs:source',
+            'requirejs:' + rtask,
             'useminPrepare',
             'imagemin',
             'copy:dist',
