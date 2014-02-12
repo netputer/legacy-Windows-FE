fd5eafc19e9c87b008d9a10c6988e360f1efacf1
NetPuter <NetPuter@Gmail.com>
2014-02-12 15:37:11 +0800

    [Polish] Add `ProjectConfig` keys into `FunctionSwitch`

--- app/javascripts/FunctionSwitch.js
+++ app/javascripts/FunctionSwitch.js
@@ -50,9 +50,24 @@
                                         Environment.get('locale') === CONFIG.enums.LOCALE_ZH_CN;
 
         Object.defineProperties(FunctionSwitch, {
+            ENABLE_APP : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_APP');
+                }
+            },
             ENABLE_APP_COMMENT : {
                 get : function () {
                     return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_APP_INSTALLED : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_APP_INSTALLED');
+                }
+            },
+            ENABLE_APP_PANEL : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_APP_PANEL');
                 }
             },
             ENABLE_APP_PERMISSION : {
@@ -68,6 +83,16 @@
             ENABLE_APP_SEARCH : {
                 get : function () {
                     return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_APP_SYS : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_APP_SYS');
+                }
+            },
+            ENABLE_APP_UPDATABLE : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_APP_UPDATABLE');
                 }
             },
             ENABLE_APP_UPGRADE : {
@@ -88,6 +113,11 @@
                                 !Environment.get('internetBar');
                 }
             },
+            ENABLE_BACKUP_RESTORE : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_BACKUP_RESTORE');
+                }
+            },
             ENABLE_BACKUP_APP_DATA : {
                 get : function () {
                     return IS_CHINESE_VERSION;
@@ -96,6 +126,26 @@
             ENABLE_CLOUD_BACKUP_RESTORE : {
                 get : function () {
                     return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_CONTACT : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_CONTACT');
+                }
+            },
+            ENABLE_CONTACT_ALL : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_CONTACT_ALL');
+                }
+            },
+            ENABLE_CONTACT_HAS_PHONE : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_CONTACT_HAS_PHONE');
+                }
+            },
+            ENABLE_CONTACT_STARRED : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_CONTACT_STARRED');
                 }
             },
             ENABLE_DORAEMON : {
@@ -107,6 +157,11 @@
             ENABLE_DUAL_SIM : {
                 get : function () {
                     return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_MUSIC : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_MUSIC');
                 }
             },
             ENABLE_MY_APPS : {
@@ -141,6 +196,21 @@
                                 !Environment.get('internetBar');
                 }
             },
+            ENABLE_PIC : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_PIC');
+                }
+            },
+            ENABLE_PIC_GALLERY : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_PIC_GALLERY');
+                }
+            },
+            ENABLE_PIC_PHONE_LIB : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_PIC_PHONE_LIB');
+                }
+            },
             ENABLE_SHARE_SET_WALLPAPER : {
                 get : function () {
                     return IS_CHINESE_VERSION;
@@ -151,9 +221,29 @@
                     return IS_CHINESE_VERSION;
                 }
             },
+            ENABLE_SMS : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_SMS');
+                }
+            },
+            ENABLE_SMS_ALL : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_SMS_ALL');
+                }
+            },
+            ENABLE_SMS_UNREAD : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_SMS_UNREAD');
+                }
+            },
             ENABLE_SUGGESTION_INSTALL : {
                 get : function () {
                     return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_TASKMANAGER_ACTION : {
+                get : function () {
+                    return !ProjectConfig.get('DISABLE_TASKMANAGER_ACTION');
                 }
             },
             ENABLE_USER_GUIDE : {
@@ -162,9 +252,24 @@
                                 !ProjectConfig.get('DISABLE_USER_GUIDE');
                 }
             },
+            ENABLE_VIDEO : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_VIDEO');
+                }
+            },
             ENABLE_WDAPK : {
                 get : function () {
                     return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_WELCOME : {
+                get : function () {
+                    return !ProjectConfig.get('HIDE_NAV_WELCOME');
+                }
+            },
+            ENABLE_WELCOME_FEED : {
+                get : function () {
+                    return !ProjectConfig.get('DISABLE_WELCOME_FEED');
                 }
             },
             IS_CHINESE_VERSION : {
@@ -175,6 +280,11 @@
             PRIVACY : {
                 get : function () {
                     return PRIVACY;
+                }
+            },
+            SHOW_FIRST_EXTENSION : {
+                get : function () {
+                    return ProjectConfig.get('SHOW_FIRST_EXTENSION');
                 }
             }
         });
