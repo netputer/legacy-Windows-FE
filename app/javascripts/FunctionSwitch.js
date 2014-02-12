946f573d4cbe8ca194b7d35b6559202994193991
NetPuter <NetPuter@Gmail.com>
2014-02-12 15:08:25 +0800

    [Polish] Reorder object keys in `FunctionSwitch`

--- app/javascripts/FunctionSwitch.js
+++ app/javascripts/FunctionSwitch.js
@@ -28,11 +28,6 @@
         var ENABLE_DEBUG = '0x00000040';
 
         Object.defineProperties(PRIVACY, {
-            RECORD_BROWSE_HISTORY : {
-                get : function () {
-                    return SETTING & RECORD_BROWSE_HISTORY;
-                }
-            },
             ENABLE_APP_UPGRADE : {
                 get : function () {
                     return SETTING & ENABLE_APP_UPGRADE;
@@ -42,6 +37,11 @@
                 get : function () {
                     return !(SETTING & ENABLE_DEBUG);
                 }
+            },
+            RECORD_BROWSE_HISTORY : {
+                get : function () {
+                    return SETTING & RECORD_BROWSE_HISTORY;
+                }
             }
         });
 
@@ -50,29 +50,7 @@
                                         Environment.get('locale') === CONFIG.enums.LOCALE_ZH_CN;
 
         Object.defineProperties(FunctionSwitch, {
-            PRIVACY : {
-                get : function () {
-                    return PRIVACY;
-                }
-            },
-            IS_CHINESE_VERSION :  {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_APP_UPGRADE : {
-                get : function () {
-                    return IS_CHINESE_VERSION &&
-                                PRIVACY.ENABLE_APP_UPGRADE &&
-                                !ProjectConfig.get('DISABLE_APP_UPGRADE');
-                }
-            },
             ENABLE_APP_COMMENT : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_APP_RECOMMEND : {
                 get : function () {
                     return IS_CHINESE_VERSION;
                 }
@@ -82,76 +60,21 @@
                     return IS_CHINESE_VERSION;
                 }
             },
+            ENABLE_APP_RECOMMEND : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
             ENABLE_APP_SEARCH : {
                 get : function () {
                     return IS_CHINESE_VERSION;
                 }
             },
-            ENABLE_DORAEMON : {
+            ENABLE_APP_UPGRADE : {
                 get : function () {
                     return IS_CHINESE_VERSION &&
-                                !ProjectConfig.get('DISABLE_DORAEMON');
-                }
-            },
-            ENABLE_PHOTO_SYNC : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_PHOTO_SYNC_DOWNLOAD : {
-                get : function () {
-                    return IS_CHINESE_VERSION &&
-                                !Environment.get('internetBar');
-                }
-            },
-            ENABLE_DUAL_SIM : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_OPTIMIZE : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_WDAPK : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_MY_APPS : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_SUGGESTION_INSTALL : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_MY_MUTIL_SOCIAL_PLATFORM : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_SHARE_SET_WALLPAPER : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_CLOUD_BACKUP_RESTORE : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_SHARE_UNINSTALL : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
-                }
-            },
-            ENABLE_BACKUP_APP_DATA : {
-                get : function () {
-                    return IS_CHINESE_VERSION;
+                                PRIVACY.ENABLE_APP_UPGRADE &&
+                                !ProjectConfig.get('DISABLE_APP_UPGRADE');
                 }
             },
             ENABLE_APP_WASH : {
@@ -165,16 +88,93 @@
                                 !Environment.get('internetBar');
                 }
             },
+            ENABLE_BACKUP_APP_DATA : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_CLOUD_BACKUP_RESTORE : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_DORAEMON : {
+                get : function () {
+                    return IS_CHINESE_VERSION &&
+                                !ProjectConfig.get('DISABLE_DORAEMON');
+                }
+            },
+            ENABLE_DUAL_SIM : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_MY_APPS : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_MY_MUTIL_SOCIAL_PLATFORM : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_OPTIMIZE : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
             ENABLE_PHOTO_DOWNLOAD_POPUP : {
                 get : function () {
                     return IS_CHINESE_VERSION &&
                                 !Environment.get('internetBar');
                 }
             },
+            ENABLE_PHOTO_SYNC : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_PHOTO_SYNC_DOWNLOAD : {
+                get : function () {
+                    return IS_CHINESE_VERSION &&
+                                !Environment.get('internetBar');
+                }
+            },
+            ENABLE_SHARE_SET_WALLPAPER : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_SHARE_UNINSTALL : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            ENABLE_SUGGESTION_INSTALL : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
             ENABLE_USER_GUIDE : {
                 get : function () {
                     return IS_CHINESE_VERSION &&
                                 !ProjectConfig.get('DISABLE_USER_GUIDE');
+                }
+            },
+            ENABLE_WDAPK : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            IS_CHINESE_VERSION : {
+                get : function () {
+                    return IS_CHINESE_VERSION;
+                }
+            },
+            PRIVACY : {
+                get : function () {
+                    return PRIVACY;
                 }
             }
         });
