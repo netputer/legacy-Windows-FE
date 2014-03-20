0ba575361005f8d25320b91e01a03520645a5ed5
lixiaopeng <lixiaopeng@wandoujia.com>
2014-03-20 15:17:12 +0800

    [Bugfix] use timeout function

--- app/javascripts/modules/taskmanager/views/TaskModuleView.js
+++ app/javascripts/modules/taskmanager/views/TaskModuleView.js
@@ -43,6 +43,7 @@
         var clearInterval = window.clearInterval;
 
         var hasRecordFPS = false;
+        var timeoutHandle;
 
         var pushNotificationView;
         var downloadHandler = function (msg) {
@@ -176,19 +177,24 @@
             },
             slideIn : function () {
 
+                clearTimeout(timeoutHandle);
                 this.show = true;
 
                 if (!hasRecordFPS) {
                     hasRecordFPS = true;
                     this.recordFPS();
                 }
-                this.$el.find('.ctn').one('webkitAnimationEnd', function () {
-                    Backbone.trigger('taskManager.showModule', 'task');
-                });
+
                 lastView = SnapPea.CurrentModule;
                 this.$el.toggleClass('hide', !this.show);
+
+                timeoutHandle = setTimeout(function () {
+                    Backbone.trigger('taskManager.showModule', 'task');
+                }, 500);
             },
             slideOut : function () {
+
+                clearTimeout(timeoutHandle);
                 if (lastView){
                     Backbone.trigger('taskManager.showModule', lastView);
                 }
