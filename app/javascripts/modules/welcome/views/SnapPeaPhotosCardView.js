4328f7d3caee5d0bbe7a8bdf92c86e02a54f1758
zhangyaochun <zhangyaochun@wandoujia.com>
2014-06-13 10:17:50 +0800

    fix dev/build bug

--- app/javascripts/modules/welcome/views/SnapPeaPhotosCardView.js
+++ app/javascripts/modules/welcome/views/SnapPeaPhotosCardView.js
@@ -17,14 +17,32 @@
         TemplateFactory,
         FeedCardView
     ) {
+        
+        var url = '383-wdj-extension://__MSG_@@extension_id__/index.html?type=game#game';
+
         var SnapPeaPhotosCardView = FeedCardView.getClass().extend({
             template : doT.template(TemplateFactory.get('welcome', 'snappea-photos')),
             className : FeedCardView.getClass().prototype.className + ' snappea-photos',
+            initialize : function () {
+
+                SnapPeaPhotosCardView.__super__.initialize.apply(this, arguments);
+
+                Object.defineProperties(this, {
+                    url : {
+                        get : function () {
+                            return url + this.model.id;
+                        }
+                    }
+                });
+            },
             render : function () {
                 this.$el.html(this.template({}));
                 return this;
             },
             clickButtonAction : function (evt) {
+
+                this.openDoraemon(this.url);
+
                 this.log({
                     action : 'photos'
                 }, evt);
--- app/javascripts/modules/welcome/views/SnapPeaWebCardView.js
+++ app/javascripts/modules/welcome/views/SnapPeaWebCardView.js
@@ -19,14 +19,32 @@
         TemplateFactory,
         FeedCardView
     ) {
+
+        var url = '380-wdj-extension://__MSG_@@extension_id__/index.html?type=game#game';
+
         var SnapPeaWebCardView = FeedCardView.getClass().extend({
             template : doT.template(TemplateFactory.get('welcome', 'snappea-web')),
             className : FeedCardView.getClass().prototype.className + ' snappea-web',
+            initialize : function () {
+
+                SnapPeaWebCardView.__super__.initialize.apply(this, arguments);
+
+                Object.defineProperties(this, {
+                    url : {
+                        get : function () {
+                            return url + this.model.id;
+                        }
+                    }
+                });
+            },
             render : function () {
                 this.$el.html(this.template({}));
                 return this;
             },
             clickButtonAction : function (evt) {
+
+                this.openDoraemon(this.url);
+
                 this.log({
                     action : 'web'
                 }, evt);
