d6b12c134350f177aba8871be3c6ad5e856938d9
lixiaopeng <lixiaopeng1025@gmail.com>
2015-01-04 15:00:59 +0800

    Revert "[Bugfix] remove unnecessary logic"
    This reverts commit 0e65c51989823abc288198f90541542ac0d723b5.

--- app/javascripts/modules/music/views/ImportMusicView.js
+++ app/javascripts/modules/music/views/ImportMusicView.js
@@ -76,6 +76,8 @@
                 musicList.on('switchSet', function (currentSet) {
                     musicList.emptyTip = i18n.music.NO_SELECTED_MUSIC_TEXT;
                     musicList.toggleEmptyTip(musicList.currentModels.length === 0);
+
+                    this.setFooterContent();
                 }, this);
 
                 musicList.on('select:change', function () {
@@ -90,17 +92,29 @@
                 return this;
             },
             parseMusics : function (resp) {
-                var newMusicIds = [];
+                var newMusics = [];
+
                 _.each(resp.body.audio, function (music) {
                     music.id = StringUtil.MD5(music.path);
-                    this.collection.add(new MusicModel(music));
-                    newMusicIds.push(music.id);
+                    newMusics.push(new MusicModel(music));
+                });
+
+                var newMusicIds = [];
+
+                _.each(newMusics, function (item) {
+                    var music = this.collection.get(item.id);
+                    if (music) {
+                        music.set(item.toJSON());
+                    } else {
+                        this.collection.add(item);
+                    }
+                    newMusicIds.push(item.id);
                 }, this);
-                musicList.addSelect(newMusicIds);
 
                 musicList.switchSet('default', function () {
                     return this.collection.models;
                 }.bind(this));
+                musicList.addSelect(newMusicIds);
             },
             selectMusics : function (type) {
                 alertWindow.show();
